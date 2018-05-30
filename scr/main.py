bfba3ffca37d10dfe74dba80b480948c755ce28d
khoidt <ekh.itd@gmail.com>
2018-05-30 15:42:07 +0200

    Correct git switch (#8)

--- scr/main.py
+++ scr/main.py
@@ -287,7 +287,8 @@ class annotation_functions(common_functions):
     repo_data = 'https://%s:%s@%s' %(self.user,
                                      self.password,
                                      self.GIT_ANNOTATED.split('//')[1])
-    sp.run(['git', 'checkout', '-b', self.branch], cwd=self.ANNOTATED_PATH)
+    # Use ['git', 'checkout', '-b', self.branch] to create branch
+    sp.run(['git', 'checkout', self.branch], cwd=self.ANNOTATED_PATH)
     sp.run(['git', 'add', '.'], cwd=self.ANNOTATED_PATH)
     sp.run(['git', 'commit', '-a','-m', message], cwd=self.ANNOTATED_PATH)
     sp.run(['git', 'push', repo_data, self.branch],
@@ -398,7 +399,6 @@ class Dashboard(common_functions):
 
 #---/ Eel functions /----------------------------------------------------------
 #
-
 eel.init('static')
 d = Dashboard()
 
